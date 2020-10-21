6454381df81668a69a6fed463fef9186fc0f9025
Marc <marc.daniel.jacques@gmail.com>
2020-10-21 12:36:19 -0400

    created instances related to the structs and classes created for steps 1 through 4

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,8 +1,18 @@
+enum Rating: String {
+    case OK = "* * *"
+    case GOOD = "* * * *"
+    case BEST = "* * * * *"
+}
+
 struct Flavor {
     let name: String
-    let rating: Int
-    
+    let rating: Rating
 }
+
+
+let favoriteFlavor = Flavor(name: "Vanilla", rating: .BEST )
+let secondFavorite = Flavor(name: "Cheesecake", rating: .BEST)
+let reallyGreatIceCream = Flavor(name: "Oreo", rating: .BEST)
 
 enum Size: Double {
     case small = 3.99
@@ -12,13 +22,16 @@ enum Size: Double {
 
 struct Cone {
     let flavor: String
-    let topping: String
-    let size : Size
+    let toppings: String
     
     func eat() {
         print("Mmmm! I love \(flavor)")
     }
 }
+
+let cone1 = Cone(flavor: "Plain", toppings: "none")
+let cone2 = Cone(flavor: "Waffle", toppings: "Toasted waffles")
+let rainboowCone = Cone(flavor: "Rainbow", toppings: "Rainbow sprinkles")
 
 struct IceCream {
     let flavor: Flavor
@@ -26,12 +39,20 @@ struct IceCream {
     let size: Size
 }
 
+let VanillaCream = IceCream(flavor: favoriteFlavor, cone: rainboowCone, size: .medium)
+let CheeseCakeIce = IceCream(flavor: secondFavorite, cone: cone2, size: .medium)
+let OreoIceCream = IceCream(flavor: reallyGreatIceCream, cone: cone1, size: .medium)
+let menu :[IceCream] = [VanillaCream, CheeseCakeIce, OreoIceCream]
+
 class IceCreamShop {
     let menu: [IceCream]
     var totalSales: Double
     
-    init(menu: [IceCream], totalSales: Double) {
+    init(menu: [IceCream]) {
         self.menu = menu
-        self.totalSales = totalSales
+        self.totalSales = 0.00
     }
 }
+
+
+
