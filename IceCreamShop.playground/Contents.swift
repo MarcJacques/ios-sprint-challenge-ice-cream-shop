b2cb1e5c751db8bc44f9bf75dd4d57fe59281845
Marc <marc.daniel.jacques@gmail.com>
2020-10-21 11:32:04 -0400

    Completed first 3 steps

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -1,1 +1,17 @@
+struct Flavor {
+    let name: String
+    let rating: Int
+    
+}
 
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
+}
+
+struct Cone {
+    let flavor: String
+    let topping: String
+    let size : Size
+}
