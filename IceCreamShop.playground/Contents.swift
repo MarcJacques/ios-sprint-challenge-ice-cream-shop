a649d972667101e61bc956ff3c5215d95d4d377b
Marc <marc.daniel.jacques@gmail.com>
2020-10-21 12:08:58 -0400

    Added method to cone struct and completed #4

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -6,12 +6,32 @@ struct Flavor {
 
 enum Size: Double {
     case small = 3.99
-    case medium = 4.99
-    case large = 5.99
+    case medium = 5.99
+    case large = 7.99
 }
 
 struct Cone {
     let flavor: String
     let topping: String
     let size : Size
+    
+    func eat() {
+        print("Mmmm! I love \(flavor)")
+    }
+}
+
+struct IceCream {
+    let flavor: Flavor
+    let cone: Cone
+    let size: Size
+}
+
+class IceCreamShop {
+    let menu: [IceCream]
+    var totalSales: Double
+    
+    init(menu: [IceCream], totalSales: Double) {
+        self.menu = menu
+        self.totalSales = totalSales
+    }
 }
